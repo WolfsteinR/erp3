298898db9ba6d3d52480aee08b7ed5f687d76b1f
WolfsteinR <wolfsz@yandex.ru>
2016-12-23 17:08:07 +0300

    From work

--- database/migrations/2016_12_08_091302_create_tasks_table.php
+++ database/migrations/2016_12_08_091302_create_tasks_table.php
@@ -18,13 +18,17 @@ class CreateTasksTable extends Migration
             $table->increments('id'); // table Id's
             $table->integer('author_id')->unsigned()->default(0);
             $table->foreign('author_id')
-                ->references('id')->on('users')
-                ->onDelete('cascade');
+                ->references('id')->on('users');
+            $table->foreign('spec_id')
+                ->references('id')->on('users');
+                //->onDelete('cascade');
             $table->string('title')->unique();
             $table->text('body'); // our tasks
             $table->string('website');
-            $table->text('desc_manager'); // comments by manager about task
+            $table->text('desc_manager')->nullable(); // comments by manager about task
             $table->enum('priority', ['low','medium','high']); // priority task
+            $table->string('time_limit')->nullable();// time limit for task from manager
+            $table->time('time')->nullable(); // time what working our task
             $table->string('slug')->unique();
             $table->boolean('active');
             $table->integer('spec_id')->unsigned()->default(0);

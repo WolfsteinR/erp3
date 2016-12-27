61e53020608131202dee23a56de052a56b367796
WolfsteinR <wolfsz@yandex.ru>
2016-12-27 09:42:03 +0300

    From work

--- database/migrations/2016_12_08_091302_create_tasks_table.php
+++ database/migrations/2016_12_08_091302_create_tasks_table.php
@@ -19,11 +19,16 @@ class CreateTasksTable extends Migration
             $table->integer('author_id')->unsigned()->default(0);
             $table->foreign('author_id')
                 ->references('id')->on('users');
+            $table->integer('spec_id')->unsigned()->default(0);
             $table->foreign('spec_id')
                 ->references('id')->on('users');
                 //->onDelete('cascade');
+            $table->integer('upload_id')->unsigned()->default(0);
+            $table->foreign('upload_id')
+                ->references('id')->on('upload');
             $table->string('title')->unique();
             $table->text('body'); // our tasks
+            $table->boolean('hide_body');
             $table->string('website');
             $table->text('desc_manager')->nullable(); // comments by manager about task
             $table->enum('priority', ['low','medium','high']); // priority task
@@ -31,7 +36,6 @@ class CreateTasksTable extends Migration
             $table->time('time')->nullable(); // time what working our task
             $table->string('slug')->unique();
             $table->boolean('active');
-            $table->integer('spec_id')->unsigned()->default(0);
             $table->timestamps();
         });
     }
